cf0f28e346b55daa2d96559444ba57c7ac03c31f
xyziemba <xy.ziemba@gmail.com>
2011-12-18 14:46:22 -0500

    Moving around a room with a viewport

--- room.py
+++ room.py
@@ -43,10 +43,13 @@ class RenderableObject(object):
     def blitToScreen(self,screen):
         screen.blit(self.sprite,self.vp.ScreenCoords(self.position).Strip())
 
-    def getRealMeasurements(self):
-        width = self.sprite.get_width() / PIXELS_PER_METER
-        height = self.sprite.get_height() / PIXELS_PER_METER
-        return (width,height)
+    def getSize(self):
+        if self.sprite == None:
+            return None
+
+        width = self.sprite.get_width()
+        height = self.sprite.get_height()
+        return mgr.Vect(mgr.Dimension(value=width,units="px"),mgr.Dimension(value=height,units="px"))
 
 class Room(Serializable):
     '''
--- roomtest.py
+++ roomtest.py
@@ -10,16 +10,26 @@ class LevelDesigner:
     def __init__(self):
         pygame.init()
 
-        surface = pygame.display.set_mode((SCREEN_PIXEL_WIDTH + WIDGET_TOOLBAR_WIDTH,SCREEN_PIXEL_HEIGHT))
-        room = rm.Room(SCREEN_REAL_WIDTH,SCREEN_REAL_HEIGHT)
+        self.surface = pygame.display.set_mode((SCREEN_PIXEL_WIDTH + WIDGET_TOOLBAR_WIDTH,SCREEN_PIXEL_HEIGHT))
+        self.room = rm.Room(SCREEN_REAL_WIDTH*10,SCREEN_REAL_HEIGHT*10)
+
+        background = self.surface.copy()
+        background.fill((255,255,255))
+        self.background = background.convert()
+        self.surface.blit(background,(0,0))
 
         box = rm.Box(pos=mgr.Vect(mgr.Dimension(unitstr="1.0 m"),mgr.Dimension(unitstr="1.0 m")))
-        box.blitToScreen(surface)
-        
+        box.blitToScreen(self.surface)
+        self.room.boxes.append(box)
+
         print box.size
 
     def main(self):
         
+        vp = mgr.ViewPort()
+        movement = mgr.Dimension(unitstr="1.0 m")
+        zero = mgr.Dimension(unitstr="0.0 m")
+
         while True:
             #for object in room.GetAllObjects():
 
@@ -30,16 +40,25 @@ class LevelDesigner:
                     if event.key == K_ESCAPE:
                         return
                     elif event.key == K_UP:
-                        pass
+                        print "Hit Up"
+                        vp.originDelta = vp.originDelta + mgr.Vect(movement,zero)
                     elif event.key == K_DOWN:
-                        pass
+                        print "Hit Down"
+                        vp.originDelta = vp.originDelta - mgr.Vect(movement,zero)
                     elif event.key == K_RIGHT:
-                        pass
+                        print "Hit Right"
+                        vp.originDelta = vp.originDelta + mgr.Vect(zero,movement)
                     elif event.key == K_LEFT:
-                        pass
+                        print "Hit Left"
+                        vp.originDelta = vp.originDelta - mgr.Vect(zero,movement)
                 elif event.type == MOUSEBUTTONDOWN:
                     pass #handle hitting the right sprite here
-                pygame.display.flip()
+
+                
+            self.surface.blit(self.background,(0,0))
+            for item in self.room.GetAllObjects():
+                item.blitToScreen(self.surface)
+            pygame.display.flip()
         
 if (__name__ == "__main__"):
     LevelDesigner().main()
