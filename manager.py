f6f208b95130b4294534d8b3fce4067988d975af
KieferO <Kiefer.Hicks@gmail.com>
2011-12-18 01:32:29 -0500

    New dimensional-y-er version of Vect

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@
 
 import sys
 import pygame
+import math
 from pygame.locals import *
 from Box2D import *
 
@@ -19,6 +20,22 @@ class Vect(object):
             raise ValueError("Incompatible units")
         return Vect(self.x + other.x, self.y + other.y, self.units)
 
+    def meters(self):
+        if units = "meters":
+            return self
+        else:
+            x = self.x / PIXELS_PER_METER
+            y = (768 - self.y) / PIXELS_PER_METER
+            return Vect(x, y, "meters")
+
+    def pixels(self):
+        if units = "pixels":
+            return self
+        else:
+            x = self.x * PIXELS_PER_METER
+            y = SCREEN_PIXEL_SIZE - (self.y * PIXELS_PER_METER)
+            return Vect(x, y, "pixels")
+
 class World(b2World):
     def __init__(self, size, gravity):
         self.size = size # [m]
@@ -29,20 +46,42 @@ class World(b2World):
         doSleep = True
         super(World, self).__init__(world, gravity, doSleep)
 
-    def ToMeters(self, vect):
-        return Vect()
+class Rect(object):
+    def __init__(self, world, pos, size, density):
+        bodyDef = b2BodyDef
+        bodyDef.position = (pos.meters().x, pos.meters().y)
+        self.body = world.CreateBody(bodyDef)
 
-class Rect(Box2D.b2Body):
-    def __init__(self, world, pos):
-        bodyDef = b2BodyDef()
-        bodyDef.position = pos
-        self = world.CreateBody(bodyDef)
+        shapeDef = b2PolygonDef()
+        shapeDef.SetAsBox(size.meters().x / 2.0, size.meters.y / 2.0)
+        shapeDef.density = density
+        shapeDef.linearDamping = AIR_RESISTANCE
+        shapeDef.friction = FRICTION
+        
+        self.body.CreateShape(shapeDef)
+        self.body.SetMassFromShape()
+
+    def GetCornersPixels(self):
+        
 
 def main():
-    w = World(Vect(10, 10, "meters"), (0, -10))
-    r = Rect(w, (5, 5))
-    print r
-    return 0
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+
+    screen.blit(background, (0, 0))
+
+    clock = pygame.time.Clock()
+
+    w = World(Vect(SCREEN_REAL_WIDTH, SCREEN_REAL_HEIGHT, "meters"), GRAVITY)
+    groundRectPos = Vect(SCREEN_REAL_WIDTH / 2.0, 0.5, "meters")
+    groundRectSize = Vect(SCREEN_REAL_WIDTH, 1.0, "meters")
+    groundRect = Rect(w, groundRectPos, groundRectSize, 0)
+
+
 
 if __name__ == '__main__':
     sys.exit(main())
